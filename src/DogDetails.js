4a6482b0d9686eda793bae7ac25f4cd91db40deb
heyimmatt <mrmcfarland@gmail.com>
2020-08-10 14:39:08 -0700

    Add DogDetails css and update page styling

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './DogDetails.css'
 
 export default function DogDetails( { getDog } ) {
   const { name } = useParams();
